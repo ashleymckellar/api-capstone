ae4b62ce478e1b2729f23765a9597fdbd6022809
Ashley McKellar <ashley.l.mckellar@gmail.com>
2023-08-06 21:35:07 -0700

    removed long description

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,12 +13,7 @@ export default function Home() {
             <img className="img-fluid rounded mx-auto d-block custom-img" src="./nalhutta2.jpeg"></img>
             
             <div className="intro-div mx-10">
-            <p className="trade px-10 custom-home">You may know Pablo from his humble beginnings as a pawn shop owner on Nal Hutta.  He has recently opened Pablo's Vehicles, the premier vehicle dealer in the galaxy!
-At Pablo's Vehicles, we specialize in providing the best vehicles from all corners of the galaxy to our customers. Whether you're a Jedi in need of a reliable vehicle to travel to remote planets or a wealthy collector looking for something rare and unique, we have something for everyone.
-Our selection of vehicles is unmatched in the galaxy, with a wide variety of ships, speeders, and other vehicles to choose from. We work directly with manufacturers and private collectors to bring you the highest quality vehicles at the best prices.
-Our experienced team of vehicle experts is dedicated to helping you find the perfect vehicle for your needs. We understand that every customer is unique, and we take the time to listen to your needs and preferences to help you find the vehicle that's right for you.
-So whether you're a seasoned pilot or a first-time buyer, come visit us at Pablo's and see what we have to offer. Our showroom is located in the Outer Rim on Nal Hutta, but we believe you'll find we are worth the trip. Our friendly staff is always ready to help you find the vehicle of your dreams.
-</p>
+           
 </div>
 
 
